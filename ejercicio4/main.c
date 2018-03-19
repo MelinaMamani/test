d83719fe8774d1e9d2de6cab5723afd21ccbb7b4
MelinaMamani <erikamelinamamani@gmail.com>
2018-03-19 12:23:49 -0300

    modificado

--- ejercicio4/main.c
+++ ejercicio4/main.c
@@ -3,5 +3,18 @@
 
 int main()
 {
+    int base, altura;
+    int superficie, perimetro;
 
+    printf("Ingrese la base del triangulo: ");
+    scanf("%d", &base);
+
+    fflush(stdin);
+    printf("Ingrese la altura: ");
+    scanf("%d", &altura);
+
+    superficie= (base*altura)/2;
+    printf("La superficie del triangulo es %d", superficie);
+
+    return 0;
 }
