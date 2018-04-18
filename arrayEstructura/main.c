e82f564ac81cf87e70ffe1069167f1471a91b39c
MelinaMamani <erikamelinamamani@gmail.com>
2018-04-18 10:17:23 -0300

    ordenamiento por 2 criterios

--- arrayEstructura/main.c
+++ arrayEstructura/main.c
@@ -1,44 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define CANT 3
+
+typedef struct{
+    int dia, mes, anio;
+}eFecha;
 
 typedef struct{
     char nombre [20];
     int legajo;
     float sueldo;
     char sexo;
+    eFecha fn;
 }eEmpleado;
 
+void ordenarPorCriterios(eEmpleado [], int);
 void mostrarEmpleado(eEmpleado);
 void mostrarEmpleados(eEmpleado [], int);
-void ordenarEmpleados(eEmpleado[], int);
+void ordenarPorNombre(eEmpleado[], int);
 
 int main()
 {
-    eEmpleado plantel [3] = {{"Juan", 1111, 1432.56, 'm'}, {"Ana", 2222, 12422.56, 'f'},{"Luis", 3333, 13432.56, 'm'}};
-    int numeros[]= {23,12,45};
+    eEmpleado plantel [CANT] = {{"Juan", 23, 853.77, 'm',{1,1,1960}}, {"Ana", 37, 1050.67, 'f',{2,2,1967}},{"Anoc", 23, 853.77, 'm',{1,1,1960}}};
 
-    /*unEmpleado.legajo= 1234;
-    strcpy(unEmpleado.nombre, "Juan");
-    unEmpleado.sueldo= 50000.50;
-    unEmpleado.sexo = 'm';*/
-
-    /*printf("\nIngrese el nombre: ");
-    gets(unEmpleado.nombre);
-    printf("Ingrese legajo: ");
-    scanf("%d", &unEmpleado.legajo);
-    printf("Ingrese el sueldo: ");
-    scanf("%f", &unEmpleado.sueldo);
-    printf("Ingrese el sexo: ");
-    fflush(stdin);
-    scanf("%c", &unEmpleado.sexo);*/
-
-    mostrarEmpleados(plantel, 3);
-    ordenarEmpleados(plantel, 3);
-    mostrarEmpleados(plantel, 3);
+    mostrarEmpleados(plantel, CANT);
+    ordenarPorCriterios(plantel, CANT);
+    mostrarEmpleados(plantel, CANT);
 
     return 0;
 }
+
+void ordenarPorCriterios(eEmpleado vec[], int tam){
+    int i, j;
+    eEmpleado aux;
+    for(i=0; i<tam; i++){
+        for(j=i+1; j<tam; j++){
+            if(vec[i].sueldo<vec[j].sueldo){
+                aux=vec[i];
+                vec[i]=vec[j];
+                vec[j]=aux;
+            }
+            else{
+                    if(vec[i].sueldo==vec[j].sueldo){
+                        if(strcmp(vec[i].nombre,vec[j].nombre)){
+                            aux=vec[i];
+                            vec[i]=vec[j];
+                            vec[j]=aux;
+                        }
+                    }
+                }
+        }
+    }
+}
+
 void mostrarEmpleados(eEmpleado vec[], int tam){
     int i;
     for(i=0; i<tam; i++){
@@ -47,13 +62,16 @@ void mostrarEmpleados(eEmpleado vec[], int tam){
 }
 
 void mostrarEmpleado(eEmpleado emp){
-    printf("\n \n Legajo: %d", emp.legajo);
+    printf("\n \nLegajo: %d", emp.legajo);
     printf("\nNombre: %s", emp.nombre);
     printf("\nSueldo: %.2f", emp.sueldo);
     printf("\nSexo: %c", emp.sexo);
+    printf("\nDia: %d", emp.fn.dia);
+    printf("\nMes: %d", emp.fn.mes);
+    printf("\nAnio: %d", emp.fn.anio);
 }
 
-void ordenarEmpleados(eEmpleado vec[], int tam){
+void ordenarPorNombre(eEmpleado vec[], int tam){
     eEmpleado aux;
     int i, j;
 
